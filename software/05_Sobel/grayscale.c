3d6b29d960593e0e86e29241c894539b7223604e
Sebastien Deriaz <sebastien.deriaz1@gmail.com>
2021-11-23 18:11:38 +0100

    added grayscales versions

--- software/05_Sobel/grayscale.c
+++ software/05_Sobel/grayscale.c
@@ -9,12 +9,15 @@
 #include <stdlib.h>
 #include <system.h>
 
+#include "optimisationsettings.h"
+
 unsigned char *grayscale_array;
 int grayscale_width = 0;
 int grayscape_height = 0;
 
 void conv_grayscale(void *picture, int width, int height) {
-    int x, y, gray;
+    int x, y;
+    unsigned short gray;
     unsigned short *pixels = (unsigned short *)picture, rgb;
     grayscale_width = width;
     grayscape_height = height;
@@ -23,13 +26,31 @@ void conv_grayscale(void *picture, int width, int height) {
     for (y = 0; y < height; y++) {
         for (x = 0; x < width; x++) {
             rgb = pixels[y * width + x];
-            /*gray = (((rgb>>11)&0x1F)<<3)*21; // red part
-            gray += (((rgb>>5)&0x3F)<<2)*72; // green part
-            gray += (((rgb>>0)&0x1F)<<3)*7; // blue part
-            gray /= 100;*/
-            gray = ((rgb >> 11) * 338 + ((rgb >> 5) & 0x3F) * 573 +
-                    (rgb & 0x1F) * 113) >>
-                   10;
+            
+#if GRAYSCALE_VERSION == 0
+            gray = (((rgb >> 11) & 0x1F) << 3) * 21;  // red part
+            gray += (((rgb >> 5) & 0x3F) << 2) * 72;  // green part
+            gray += (((rgb >> 0) & 0x1F) << 3) * 7;   // blue part
+            gray /= 100;
+#elif GRAYSCALE_VERSION == 1
+            gray = ((rgb >> 8) & 0xF8) * 77;    // red
+            gray += ((rgb >> 3) & 0xFC) * 151;  // green
+            gray += (rgb << 3 & 0xF8) * 28;     // blue
+            gray = gray >> 8;
+#elif GRAYSCALE_VERSION == 2
+            gray = ((rgb >> 8) & 0xF8) * 77;    // red
+            gray += ((rgb >> 3) & 0xFC) * 151;  // green
+            gray += (rgb & 0x1F) * 224;         // blue
+            gray = gray >> 8;
+
+#elif GRAYSCALE_VERSION == 3
+            gray = (rgb >> 2) & 0x3E00;
+            gray += (rgb << 4) & 0x7E00;  // green
+            gray += (rgb << 8) & 0x1F00;  // blue
+            gray = gray >> 8;
+#else
+
+#endif
             IOWR_8DIRECT(grayscale_array, y * width + x, gray);
         }
     }
